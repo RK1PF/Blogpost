da6ffd7257ceba3d30509611180222740301e4d7
RK1PF <graffe.raiha@gmail.com>
2022-01-06 15:58:13 +0100

    Update to use Mongodb, Mongoose in local

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
+const { query } = require("express");
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true
+})
+const postSchema = {
+  uuid: Number,
+  slug: String,
+  link: String,
+  title: String,
+  content: String,
+  date: Date,
+}
+const Post = mongoose.model("Post",postSchema);
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -10,7 +25,6 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
 const app = express();
 
 app.set("view engine", "ejs");
@@ -19,10 +33,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    content: homeStartingContent,
-    posts: posts,
-  });
+  // mongoose to find all
+  Post.find({}, (err,posts)=>{
+    posts.forEach(e => {
+      e.date = e.date.toLocaleDateString("fr-FR");
+    });
+    res.render("home",{
+      content: homeStartingContent,
+      posts: posts
+    })
+  })
 });
 app.get("/about", (req, res) => {
   res.render("about", {
@@ -38,31 +58,31 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 app.post("/compose", (req, res) => {
-  const post = {
-    id: posts.length,
-    slug: req.body.title.replaceAll(" ", "-").toLowerCase(),
+  const slug = req.body.title.replaceAll(" ", "-").toLowerCase();
+  const id =  Math.floor(Math.random()*987);
+  const post = new Post({
+    uuid: id,
+    slug: slug,
+    link: `/posts/${id}-${slug}`,
     title: req.body.title,
     content: req.body.post,
     date: new Date().toLocaleDateString("fr-FR"),
-  };
-  posts.push(post);
+  });
+  post.save((err)=>{
+    if (err) res.redirect("/");
+  });
   res.redirect("/");
 });
 app.get("/posts/:postId-:postSlug", (req, res) => {
   const id = req.params.postId;
   const slug = req.params.postSlug;
-  posts.forEach((post) => {
-    if (id == post.id && slug === post.slug) {
-      const title = post.title;
-      const content = post.content;
-      const date = post.date;
-      res.render("post", {
-        title: title,
-        content: content,
-        date: date,
-      });
-    }
-  });
+  Post.findOne({uuid: id, slug: slug}, (err,post)=>{
+    res.render("post", {
+            title: post.title,
+            content: post.content,
+            date: post.date,
+          });
+  })
 });
 
 app.listen(987, function () {
